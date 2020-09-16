ad82e0b4b88df01ffdccadcc5e5ed62b95af704d
daiyanan1992 <450176668@qq.com>
2020-09-16 17:06:57 +0800

    这是我第一个代码

--- Stu_01/Test01.py
+++ Stu_01/Test01.py
@@ -1,1 +1,14 @@
-print('这是我git的第一个代码')
+# print('这是我git的第一个代码')
+
+#写函数，检查传入的列表的长度，如果大于2，那么仅保留两个长度的值，并打印出来
+
+def listCheck(list1):
+    if len(list1)>2:
+        return list1[0:2]
+    else:
+        return list1[::]
+
+
+list=[1]
+a=listCheck(list)
+print(a)
